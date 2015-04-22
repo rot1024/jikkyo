4cc2423075be97b5bd69ceee1948766ec7c343a1
rot1024 <rot1024@gmail.com>
2015-04-23 00:29:00 +0900

    Implement titlebar switch methods and button click events

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@
   var gui = require("nw.gui");
   var win = gui.Window.get();
 
-  var Titlebar = require("./Widget/Titlebar");
-
   window.document.body.setAttribute("data-platform", process.platform);
   window.addEventListener("DOMContentLoaded", () => {
 
     window.addEventListener("keydown", e => {
       if (e.keyCode === 123) win.showDevTools();
+    });
+
+    var titlebar = document.querySelector("jikkyo-titlebar");
+    window.addEventListener("click", () => {
+      titlebar.toggleTitlebar();
     });
 
     win.setTransparent(true);
