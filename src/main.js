e587bf240bef1b5479bbd78e88e27cd007babe01
rot1024 <rot1024@gmail.com>
2015-04-22 17:46:41 +0900

    Add Viewer component

--- src/main.js
+++ src/main.js
@@ -6,23 +6,11 @@
 
   var Titlebar = require("./Widget/Titlebar");
 
+  window.document.body.setAttribute("data-platform", process.platform);
   window.addEventListener("DOMContentLoaded", () => {
-
-    window.document.body.setAttribute("data-platform", process.platform);
 
     window.addEventListener("keydown", e => {
       if (e.keyCode === 123) win.showDevTools();
-    });
-
-    var titlebar = new Titlebar();
-    titlebar.attach(window.document.getElementById("titlebar"));
-
-    var viewer = window.document.getElementById("viewer");
-    viewer.setAttribute("width", win.width);
-    viewer.setAttribute("height", win.height);
-    win.on("resize", (w, h) => {
-      viewer.setAttribute("width", w);
-      viewer.setAttribute("height", h);
     });
 
     win.setTransparent(true);
