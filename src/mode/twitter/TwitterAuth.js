54f96b6fd596d25bad4c7f23f62e8feed2f1741b
rndom <rndom.hack@gmail.com>
2015-05-09 04:43:29 +0900

    add TwitterAuth.js

---
+++ src/mode/twitter/TwitterAuth.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var oauth = require("oauth");
+
+class TwitterAuth {
+  constructor(options) {
+    options = options || {};
+    this.consumerKey = options.consumerKey;
+    this.consumerSecret = options.consumerSecret;
+    this.requestURL = options.requestURL || "https://api.twitter.com/oauth/request_token";
+    this.accessURL = options.requestURL || "https://api.twitter.com/oauth/access_token";
+    this.oAuthURL = options.oAuthURL || "https://api.twitter.com/oauth";
+  }
+
+  getAuthorizeURL() {
+    var deferred = Promise.defer();
+
+    var client = new oauth.OAuth(
+      this.requestURL,
+      this.accessURL,
+      this.consumerKey,
+      this.consumerSecret,
+      "1.0",
+      null,
+      "HMAC-SHA1"
+    );
+
+    client.getOAuthRequestToken(((err, oAuthToken, oAuthTokenSecret) => {
+      if (err) deferred.reject(err);
+
+      this.oAuthToken = oAuthToken;
+      this.oAuthTokenSecret = oAuthTokenSecret;
+
+      deferred.resolve(`${this.oAuthURL}/authorize?oauth_token=${oAuthToken}`);
+    }).bind(this));
+
+    return deferred.promise;
+  }
+
+  getAccessToken(oauthVerifier) {
+    var deferred = Promise.defer();
+
+    var client = new oauth.OAuth(
+      this.requestURL,
+      this.accessURL,
+      this.consumerKey,
+      this.consumerSecret,
+      "1.0",
+      null,
+      "HMAC-SHA1"
+    );
+
+    client.getOAuthAccessToken(
+      this.oAuthToken,
+      this.oAuthTokenSecret,
+      oauthVerifier,
+      ((err, oAuthAccessToken, oAuthAccessTokenSecret) => {
+      if (err) deferred.reject(err);
+
+      deferred.resolve({
+        accessToken: oAuthAccessToken,
+        accessTokenSecret: oAuthAccessTokenSecret
+      });
+    }).bind(this));
+
+    return deferred.promise;
+  }
+}
+
+module.exports = TwitterAuth;
