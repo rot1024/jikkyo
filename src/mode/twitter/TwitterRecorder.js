c0fa7bd1d1be615ba9c3b01ec2da07525f691f6d
rndom <rndom.hack@gmail.com>
2015-05-16 20:01:03 +0900

    Modify formatDate calculation

--- src/mode/twitter/TwitterRecorder.js
+++ src/mode/twitter/TwitterRecorder.js
@@ -103,19 +103,17 @@ module.exports = (() => {
 
     _formatDate(date, format) {
       date = date || new Date();
-      format = format || 'YYYY-MM-DD hh:mm:ss.SSS';
+      format = format || "YYYY-MM-DD hh:mm:ss.SSS";
       format = format.replace(/YYYY/g, date.getFullYear());
-      format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
-      format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2));
-      format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2));
-      format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
-      format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
-      if (format.match(/S/g)) {
-        let milliSeconds = ('00' + date.getMilliseconds()).slice(-3);
-        let length = format.match(/S/g).length;
-        for (var i = 0; i < length; i++)
-          format = format.replace(/S/, milliSeconds.substring(i, i + 1));
-      }
+      format = format.replace(/MM/g, ("0" + (date.getMonth() + 1)).slice(-2));
+      format = format.replace(/DD/g, ("0" + date.getDate()).slice(-2));
+      format = format.replace(/hh/g, ("0" + date.getHours()).slice(-2));
+      format = format.replace(/mm/g, ("0" + date.getMinutes()).slice(-2));
+      format = format.replace(/ss/g, ("0" + date.getSeconds()).slice(-2));
+      format = format.replace(/S+/g, match => {
+        if (match.length > 3) return match;
+        return ("00" + date.getMilliseconds()).slice(-3).slice(0, match.length);
+      });
       return format;
     }
 
