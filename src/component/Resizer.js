d339c43b54551f5ac79beb6c22705bb55fcd82ab
rot1024 <rot1024@gmail.com>
2015-06-24 21:48:50 +0900

    Refactor resizer

--- src/component/Resizer.js
+++ src/component/Resizer.js
@@ -1,3 +1,4 @@
+/* global Kefir */
 (() => {
   "use strict";
 
@@ -5,11 +6,22 @@
   var doc = document.currentScript.ownerDocument;
 
   var cursors = [
-    {name: "ew-resize", c: ["w", "e"] },
-    {name: "ns-resize", c: ["n", "s"] },
-    {name: "nwse-resize", c: ["nw", "se"] },
-    {name: "nesw-resize", c: ["ne", "sw"] }
+    ["ew-resize", ["w", "e"]],
+    ["ns-resize", ["n", "s"]],
+    ["nwse-resize", ["nw", "se"]],
+    ["nesw-resize", ["ne", "sw"]]
   ];
+
+  var getCursorName = function(id) {
+    var name = null;
+    cursors.some(c => {
+      if (c[1].indexOf(id) >= 0) {
+        name = c[0];
+        return true;
+      }
+    });
+    return name;
+  };
 
   var resizer = class extends HTMLElement {
 
@@ -30,101 +42,65 @@
         maximized = false;
       }).bind(this));
 
-      var drag = {
-        dragging: false,
-        x: 0,
-        y: 0,
-        oldX: 0,
-        oldY: 0,
-        horizontal: 0, // 0: west,  1: middle, 2: east
-        vertical: 0    // 0: north, 1: middle, 2: south
-      };
+      [
+        ["nw", "n", "ne"],
+        ["w", null, "e"],
+        ["sw", "s", "se"]
+      ]
+      .forEach((el, ver) => el.forEach((el, hor) => {
+        if (!el) return;
 
-      var mousemove = e => {
-        if (!drag.dragging) {
-          window.removeEventListener("mousemove", mousemove);
-          return;
-        }
-
-        drag.x = e.clientX;
-        drag.y = e.clientY;
-      };
-
-      var mouseup = () => {
-        drag.dragging = false;
-        window.removeEventListener("mousemove", mousemove);
-        window.removeEventListener("mouseup", mouseup);
-        window.document.body.style.cursor = null;
-        indicator.classList.remove("on");
-      };
-
-      var tick = () => {
-        if (!drag.dragging) return;
-
-        var diffX = drag.x - drag.oldX,
-            diffY = drag.y - drag.oldY,
-            width = 0, height = 0, x = 0, y = 0;
-
-        if (drag.horizontal !== 1)
-          width = diffX * (drag.horizontal === 0 ? -1 : 1);
-        if (drag.vertical !== 1)
-          height = diffY * (drag.vertical === 0 ? -1 : 1);
-
-        if (drag.horizontal === 0)
-          x = diffX;
-        if (drag.vertical === 0)
-          y = diffY;
-
-        if (x || y)
-          window.WindowWrapper.moveBy(x, y);
-        if (width || height)
-          window.WindowWrapper.resizeTo(
-            window.WindowWrapper.width + width,
-            window.WindowWrapper.height + height
-          );
-
-        drag.oldX = drag.x - (drag.horizontal === 0 && !window.WindowWrapper.clickthrough ? diffX : 0);
-        drag.oldY = drag.y - (drag.vertical === 0 && !window.WindowWrapper.clickthrough ? diffY : 0);
-
-        window.requestAnimationFrame(tick);
-      };
-
-      var on = (t, hor, ver, str) => {
-        if (t === null) return;
-
-        t.addEventListener("mousedown", e => {
-          if (maximized) return;
+        Kefir.fromEvents(root.getElementById(el), "mousedown")
+        .filter(() => !maximized)
+        .map(e => {
+          e.preventDefault();
           e.stopPropagation();
+          return e;
+        })
+        .flatMap(() =>
+          Kefir.fromEvents(window, "mousemove")
+          .takeUntilBy(Kefir.fromEvents(window, "mouseup"))
+          .map(e => ({ type: "mousemove", x: e.screenX, y: e.screenY }))
+          .merge(Kefir.constant({ type: "mousedown" }))
+          .beforeEnd(() => ({ type: "mouseup" })))
+        .onValue(v => {
+          if (v.type === "mousedown") {
+            window.document.body.style.cursor = getCursorName(el);
+            indicator.classList.add("on");
+            return;
+          }
 
-          drag.x = drag.oldX = e.clientX;
-          drag.y = drag.oldY = e.clientY;
-          drag.horizontal = hor;
-          drag.vertical = ver;
-          window.addEventListener("mousemove", mousemove);
-          window.addEventListener("mouseup", mouseup);
-          drag.dragging = true;
+          if (v.type === "mouseup") {
+            window.document.body.style.cursor = null;
+            indicator.classList.remove("on");
+            return;
+          }
 
-          var cursorName = "";
-          cursors.some(c => {
-            if (c.c.indexOf(str) >= 0) {
-              cursorName = c.name;
-              return true;
-            }
-          });
+          var win = window.WindowWrapper,
+              x = 0, y = 0, w = 0, h = 0;
 
-          if (cursorName)
-            window.document.body.style.cursor = cursorName;
+          if (hor === 0) {
+            x = v.x - win.x;
+            w = -x;
+            if (win.width + w < win.minWidth)
+              x = w = 0;
+          }
 
-          indicator.classList.add("on");
+          if (ver === 0) {
+            y = v.y - win.y;
+            h = -y;
+            if (win.height + h < win.minHeight)
+              y = h = 0;
+          }
 
-          window.requestAnimationFrame(tick);
+          if (hor === 2) w = v.x - win.right;
+          if (ver === 2) h = v.y - win.bottom;
+
+          if (x || y) win.moveBy(x, y);
+          if (w || h) win.resizeBy(w, h);
         });
 
-        t.addEventListener("click", e => e.stopPropagation());
-      };
-
-      [["nw", "n", "ne"], ["w", null, "e"], ["sw", "s", "se"]]
-        .forEach((e, i) => e.forEach((e, j) => on(root.getElementById(e), j, i, e)));
+      }));
     }
 
   };
