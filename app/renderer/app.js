d43467fdab15007355560c1b3b16be2dc6289f00
rot1024 <rot1024@gmail.com>
2017-04-01 01:34:52 +0900

    add titlebar and window-border to app

--- app/renderer/app.js
+++ app/renderer/app.js
@@ -1,4 +1,6 @@
 import React from "react";
+import TitleBar from "./components/molecures/titlebar";
+import WindowBorder from "./components/atoms/window-border";
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.PureComponent {
@@ -10,6 +12,8 @@ export default class App extends React.PureComponent {
   render() {
     return (
       <div>
+        <WindowBorder visible />
+        <TitleBar title="jikkyo" visible />
         <h1>Hello!</h1>
       </div>
     );
