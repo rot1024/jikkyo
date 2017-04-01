724ad40540479b57247c0f0aaff25b2775dba4e4
rot1024 <rot1024@gmail.com>
2017-04-01 16:29:58 +0900

    move electron-packager config on package.json

--- tasks/package.js
+++ tasks/package.js
@@ -3,45 +3,41 @@
 const os = require("os");
 const packager = require("electron-packager");
 const argv = require("minimist")(process.argv.slice(2));
-const del = require("del");
 const pkg = require("../package.json");
 
 const shouldBuildAll = argv.all || false;
 
 const options = {
+  appCategoryType: pkg.electron.categoryType,
+  appCopyright: pkg.electron.copyright,
   appVersion: pkg.version,
   asar: true,
   dir: ".",
   electronVersion: pkg.devDependencies.electron.replace(/^\^/, ""),
-  icon: pkg.electronIcon,
+  icon: pkg.electron.icon,
   ignore: [
     /^\/(?!build|package.json$)/
   ],
-  name: pkg.productName || pkg.name,
-  out: "dist"
+  name: pkg.electron.name || pkg.name,
+  out: "dist",
+  overwrite: true
 };
 
-del("dist")
-  .then(() => {
-    const archs = shouldBuildAll ? ["ia32", "x64"] : [os.arch()];
-    const platforms = shouldBuildAll ? ["linux", "win32", "darwin"] : [os.platform()];
-    platforms.forEach(platform => {
-      archs.forEach(arch => {
-        console.log(`${platform} ${arch} start`);
-        pack(platform, arch).then(
-          () => console.log(`${platform} ${arch} finish`),
-          err => {
-            if (!err) return;
-            console.log(`${platform} ${arch} error`);
-            throw err;
-          }
-        );
-      });
-    });
-  })
-  .catch(err => {
-    console.error(err);
+const archs = shouldBuildAll ? ["ia32", "x64"] : [os.arch()];
+const platforms = shouldBuildAll ? ["linux", "win32", "darwin"] : [os.platform()];
+platforms.forEach(platform => {
+  archs.forEach(arch => {
+    console.log(`${platform} ${arch} start`);
+    pack(platform, arch).then(
+      () => console.log(`${platform} ${arch} finish`),
+      err => {
+        if (!err) return;
+        console.log(`${platform} ${arch} error`);
+        throw err;
+      }
+    );
   });
+});
 
 function pack(platform, arch) {
   if (platform === "darwin" && arch === "ia32") {
