76164401d0596769d5b699b74713433853944e29
rot1024 <aayhrot@gmail.com>
2019-10-05 19:44:56 +0900

    set up global styles

---
+++ src/styles.tsx
@@ -0,0 +1,28 @@
+import { css } from "@emotion/core";
+
+const globalStyles = css`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+      "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html,
+  body {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  html {
+    position: fixed;
+  }
+`;
+
+export default globalStyles;
