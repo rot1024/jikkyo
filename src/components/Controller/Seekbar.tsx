5e21bebf035d3a6761917ecc8f70ac2c303ecda0
rot1024 <aayhrot@gmail.com>
2019-10-12 03:39:36 +0900

    remake seekbar styles

--- src/components/Controller/Seekbar.tsx
+++ src/components/Controller/Seekbar.tsx
@@ -1,11 +1,14 @@
 /** @jsx jsx */
-import React, { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 import { css, jsx } from "@emotion/core";
+
+import { useDrag, useHover } from "./useDrag";
 
 export interface Props {
   className?: string;
   value?: number;
   max?: number;
+  buffered?: [number, number][];
   disabled?: boolean;
   onChange?: (value: number) => void;
 }
@@ -14,73 +17,141 @@ const SeekBar: React.FC<Props> = ({
   className,
   value = 0,
   max = 0,
+  buffered,
   disabled,
   onChange
 }) => {
-  const handleChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (onChange) {
-        onChange(parseFloat(e.currentTarget.value));
-      }
-    },
-    [onChange]
+  const ref = useRef<HTMLInputElement>(null);
+
+  const [isDragging, handleDragStart] = useDrag(
+    ref,
+    useCallback(
+      ({ x }: { x: number }) => {
+        if (onChange) {
+          onChange(Math.min(1, Math.max(0, x)) * max);
+        }
+      },
+      [max, onChange]
+    ),
+    disabled
   );
-  const handleClick = useCallback((e: React.MouseEvent<HTMLInputElement>) => {
-    e.currentTarget.blur();
-  }, []);
+
+  const [hovered, hoveredX] = useHover(ref, disabled);
 
   return (
-    <div
-      className={className}
-      css={css`
-        position: relative;
-      `}
-    >
-      <input
-        type="range"
-        value={value}
-        max={max}
-        min={0}
-        disabled={disabled}
-        onChange={handleChange}
-        onClick={handleClick}
-        css={css`
-          width: 100%;
-          position: absolute;
-          top: 50%;
-          margin-top: -5px;
-          height: 10px;
-          appearance: none;
-          outline: none;
-          background: #ffffff20;
-
-          &::-webkit-slider-thumb {
-            width: 18px;
-            height: 18px;
-            background: #aaa;
-            border: 1px solid #333;
-            border-radius: 50%;
-            cursor: pointer;
-            transition: background 0.2s ease-in-out;
-            appearance: none;
-
-            &:hover {
-              background: #bbb;
-            }
-
-            &:active {
-              background: #ccc;
-            }
-          }
-
-          &:disabled::-webkit-slider-thumb {
-            background: #666;
-            cursor: default;
-          }
-        `}
-      />
+    <div className={className} css={wrapperStyles}>
+      <div
+        ref={ref}
+        css={rangeStyles(!!disabled)}
+        onMouseDown={handleDragStart}
+        onTouchStart={handleDragStart}
+      >
+        <div css={backgroundStyles}>
+          <div
+            css={hoverBarStyles(hovered)}
+            style={{ width: (max === 0 ? 0 : hoveredX * 100) + "%" }}
+          />
+          {buffered &&
+            buffered.map(([s, e], i) => {
+              return (
+                <div
+                  key={i}
+                  css={bufferedBarStyles}
+                  style={{
+                    left: (max === 0 ? 0 : (s / max) * 100) + "%",
+                    width: (max === 0 ? 0 : ((e - s) / max) * 100) + "%"
+                  }}
+                />
+              );
+            })}
+          <div
+            css={barStyles}
+            style={{
+              width: (max === 0 ? 0 : (value / max) * 100) + "%"
+            }}
+          />
+        </div>
+        <div
+          css={thumbStyles(hovered || isDragging, !!disabled)}
+          style={{
+            left: (max === 0 ? 0 : (value / max) * 100) + "%"
+          }}
+          onMouseDown={handleDragStart}
+          onTouchStart={handleDragStart}
+        />
+      </div>
     </div>
   );
 };
 
 export default SeekBar;
+
+const wrapperStyles = css`
+  user-select: none;
+  position: relative;
+`;
+
+const rangeStyles = (disabled: boolean) => css`
+  user-select: none;
+  width: 100%;
+  height: 10px;
+  position: absolute;
+  top: 50%;
+  margin-top: -5px;
+  border-radius: 3px;
+  cursor: ${disabled ? "default" : "pointer"};
+`;
+
+const backgroundStyles = css`
+  user-select: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #ffffff20;
+  border-radius: 3px 0 0 3px;
+  overflow: hidden;
+`;
+
+const barStyles = css`
+  user-select: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  background-color: #ff9d00;
+`;
+
+const bufferedBarStyles = css`
+  user-select: none;
+  position: absolute;
+  left: 0;
+  height: 100%;
+  background-color: #ccc;
+  transition: background-color 0.2s ease;
+`;
+
+const hoverBarStyles = (hovered: boolean) => css`
+  user-select: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  background-color: ${hovered ? "#ccc" : "transparent"};
+  transition: background-color 0.2s ease;
+`;
+
+const thumbStyles = (hovered: boolean, disabled: boolean) => css`
+  user-select: none;
+  position: absolute;
+  top: 50%;
+  width: 18px;
+  height: 18px;
+  background-color: #ff9d00;
+  border-radius: 50%;
+  cursor: ${disabled ? "default" : "pointer"};
+  transition: transform 0.2s ease, background-color 0.2s ease, opacity 0.2s ease;
+  transform: translate(-50%, -50%) scale(${hovered ? "1" : "0"});
+  opacity: ${hovered ? "100%" : "0%"};
+`;
